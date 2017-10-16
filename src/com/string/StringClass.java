5f29b72c5879d269fe403a3f479927068eeebaa5
sahilpwr <sahil.sandypwr@gmail.com>
2017-10-16 14:55:23 -0400

    String

--- src/com/string/StringClass.java
+++ src/com/string/StringClass.java
@@ -1,6 +1,6 @@
 package com.string;
 
-import java.io.StringBufferInputStream;
+import java.io.ByteArrayInputStream;
 
 public class StringClass {
 
@@ -39,6 +39,82 @@ public class StringClass {
 		
 		//String Buffer & Builder Class methods
 		//append() insert() delete() replace() reverse() capacity()
+		
+		//String Methods:
+		
+		String java="Java Tutorial";
+		String javaEE="Java Enterprise";
+		String javaCase="java entrerprise";
+		
+		//charAt()
+		System.out.println(java.charAt(8));
+		
+		//compareTo()
+		System.out.println(java.compareTo(javaEE));
+		
+		//concat
+		System.out.println(java.concat(javaEE));
+		
+		//contains
+		System.out.println(java.contains("Java"));
+		System.out.println(java.contains("java"));
+		
+		//equals()
+		System.out.println(java.equals(javaEE));
+		
+		//equalsIgnoreCase()
+		System.out.println(javaEE.equalsIgnoreCase(javaCase));
+		
+		//getBytes
+		byte []bytearray=java.getBytes();
+		for(byte j:bytearray)
+		{
+			System.out.println(j);
+		}
+		
+		
+		//isEmpty()
+		System.out.println(java.isEmpty());
+		
+		//length()
+		System.out.println(java.length());
+		
+		//replace()
+		System.out.println(java.replace('a', 'b'));
+		
+		//replaceAll()
+		System.out.println(java.replaceAll("av","bb"));
+		
+		//split()
+		String[] split=java.split("\\s");
+		for(String w:split)
+		{
+			System.out.println(w);
+		}
+		
+		//substring()
+		System.out.println(java.substring(5));
+		System.out.println(java.substring(0, 5));
+		
+		//toCharArray()
+		char[] chararray=java.toCharArray();
+		for(char w:chararray)
+		{
+			System.out.print(w);
+		}
+		//trim()
+		String trim=" Java ";
+		System.out.println(trim.trim());
+		
+		//valueOf() //string representation of given value
+		int val=50;
+		String string=String.valueOf(50);
+		System.out.println(string);
+		
+		//lower case & upper case
+		System.out.println(java.toUpperCase());
+		System.out.println(javaEE.toLowerCase());
+	
 		
 	}
 
